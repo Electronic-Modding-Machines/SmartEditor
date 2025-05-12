92f4037292a03f7b04288287860f0bf4f80da11a
아티 <keh09523@gmail.com>
2025-05-12 23:42:15 +0900

    feat: Validate Ncalc System

--- SmartEditor/MoreFunction.cs
+++ SmartEditor/MoreFunction.cs
@@ -1,4 +1,10 @@
-﻿using JALib.Core;
+﻿using System.Data;
+using ADOFAI;
+using ADOFAI.LevelEditor.Controls;
+using JALib.Core;
+using JALib.Core.Patch;
+using NCalc;
+using UnityEngine;
 
 namespace SmartEditor;
 
@@ -9,5 +15,53 @@ public class MoreFunction() : Feature(Main.Instance, nameof(MoreFunction)) {
     
     protected override void OnDisable() {
         base.OnDisable();
+    }
+
+    [JAPatch(typeof(PropertyControl_Text), "Validate", PatchType.Replace, false)]
+    public static string PatchEntry(PropertyControl_Text __instance)
+    {
+        if (__instance.propertyInfo == null)
+            return __instance.inputField.text;
+        if (__instance.propertyInfo.type == PropertyType.Float)
+        {
+            float result = 1f;
+            float num;
+            if (float.TryParse(__instance.inputField.text, out result))
+            {
+                num = __instance.propertyInfo.Validate(result);
+            }
+            else
+            {
+                try
+                {
+                    num = __instance.propertyInfo.Validate(RDEditorUtils.DecodeFloat(new Expression(__instance.inputField.text).Evaluate()));
+                }
+                catch
+                {
+                    num = (float) __instance.propertyInfo.value_default;
+                }
+            }
+            return num.ToString();
+        }
+        if (__instance.propertyInfo.type != PropertyType.Int && __instance.propertyInfo.type != PropertyType.Tile)
+            return __instance.inputField.text;
+        float result1;
+        int num1;
+        if (float.TryParse(__instance.inputField.text, out result1))
+        {
+            num1 = __instance.propertyInfo.Validate(Mathf.RoundToInt(result1));
+        }
+        else
+        {
+            try
+            {
+                num1 = RDEditorUtils.DecodeInt(new Expression(__instance.inputField.text).Evaluate());
+            }
+            catch
+            {
+                num1 = (int) __instance.propertyInfo.value_default;
+            }
+        }
+        return num1.ToString();
     }
 }
