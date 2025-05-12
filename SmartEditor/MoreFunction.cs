5079b232b3fb6766d884a83d85b07a5e0d424fcf
아티 <keh09523@gmail.com>
2025-05-12 22:18:28 +0900

    feat: Setup MoreFunction

---
+++ SmartEditor/MoreFunction.cs
@@ -0,0 +1,13 @@
+﻿using JALib.Core;
+
+namespace SmartEditor;
+
+public class MoreFunction() : Feature(Main.Instance, nameof(MoreFunction)) {
+    protected override void OnEnable() {
+        base.OnEnable();
+    }
+    
+    protected override void OnDisable() {
+        base.OnDisable();
+    }
+}
