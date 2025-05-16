9fe263e01d5983292ec1eaea6a61f39629fca866
Teo Han <meteory.kr@gmail.com>
2025-05-16 18:10:41 +0900

    fix: Vector2PatchEntry

--- SmartEditor/FixLoad/CustomSaveState/FixPrivateMethod.cs
+++ SmartEditor/FixLoad/CustomSaveState/FixPrivateMethod.cs
@@ -2,6 +2,7 @@
 using ADOFAI;
 using ADOFAI.LevelEditor.Controls;
 using JALib.Tools;
+using PropertyInfo = System.Reflection.PropertyInfo;
 
 namespace SmartEditor.FixLoad.CustomSaveState;
 
@@ -14,8 +15,15 @@ public static class FixPrivateMethod {
     public static FieldInfo copiedTrackColorField = typeof(scnEditor).Field("copiedHitsound");
     public static MethodInfo convertEmptyToNaNMethod = typeof(PropertyControl_Vector2).Method("ConvertEmptyToNaN");
     public static MethodInfo convertNaNToEmptyMethod = typeof(PropertyControl_Vector2).Method("ConvertNaNToEmpty");
-
-
+    public static MethodInfo updateSteamCallbacksMethod = typeof(scnEditor).Method("UpdateSteamCallbacks"); 
+    public static MethodInfo quitToMenuMethod = typeof(scnEditor).Method("QuitToMenu");
+    public static MethodInfo updateSelectedFloorMethod = typeof(scnEditor).Method("UpdateSelectedFloor");
+    public static MethodInfo ottoUpdateMethod = typeof(scnEditor).Method("OttoUpdate");
+    public static MethodInfo saveBackupMethod = typeof(scnEditor).Method("SaveBackup");
+    public static MethodInfo handleKeyboardActionsMethod = typeof(scnEditor).Method("HandleKeyboardActions");
+    public static MethodInfo handleMouseActionsMethod = typeof(scnEditor).Method("HandleMouseActions");
+    public static PropertyInfo pausedProperty = typeof(scnEditor).Property("paused");
+    
     public static LevelEvent copiedHitsound => (LevelEvent) copiedHitsoundField.GetValue(scnEditor.instance);
     public static LevelEvent copiedTrackColor => (LevelEvent) copiedTrackColorField.GetValue(scnEditor.instance);
 
@@ -34,9 +42,38 @@ public static class FixPrivateMethod {
         return (LevelEvent) CopyEventMethod.Invoke(scnEditor.instance, [@event, seqId]);
     }
     public static string ConvertEmptyToNaN(string s) {
-        return (string)convertEmptyToNaNMethod.Invoke(scnEditor.instance, [s]);
+        var instance = UnityEngine.Object.FindObjectOfType<PropertyControl_Vector2>();
+        return (string)convertEmptyToNaNMethod.Invoke(instance, new object[] { s });
     }
+
+
     public static string ConvertNaNToEmpty(string s) {
-        return (string)convertNaNToEmptyMethod.Invoke(scnEditor.instance, [s]);
+        var instance = UnityEngine.Object.FindObjectOfType<PropertyControl_Vector2>();
+        return (string)convertNaNToEmptyMethod.Invoke(instance, new object[] { s });
+    }
+
+    public static void UpdateSteamCallbacks() {
+        updateSteamCallbacksMethod.Invoke(scnEditor.instance, null);
+    }
+    public static void QuitToMenu() {
+        quitToMenuMethod.Invoke(scnEditor.instance, null);
+    }
+    public static void UpdateSelectedFloor() {
+        updateSelectedFloorMethod.Invoke(scnEditor.instance, null);
+    }
+    public static void OttoUpdate() {
+        ottoUpdateMethod.Invoke(scnEditor.instance, null);
+    }
+    public static void SaveBackup() {
+        saveBackupMethod.Invoke(scnEditor.instance, null);
+    }
+    public static void HandleKeyboardActions() {
+        handleKeyboardActionsMethod.Invoke(scnEditor.instance, null);
+    }
+    public static void HandleMouseActions() {
+        handleMouseActionsMethod.Invoke(scnEditor.instance, null);
+    }
+    public static bool paused() {
+        return (bool) pausedProperty.GetValue(scnEditor.instance);
     }
 }
