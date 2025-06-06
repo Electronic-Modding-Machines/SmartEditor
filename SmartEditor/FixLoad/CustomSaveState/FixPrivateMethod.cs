1af05f86b8aa73bf482f05fc23c2ce393c6ed46c
Teo Han <meteory.kr@gmail.com>
2025-06-06 14:37:50 +0900

    feat: DeleteEditorSpeed

--- SmartEditor/FixLoad/CustomSaveState/FixPrivateMethod.cs
+++ SmartEditor/FixLoad/CustomSaveState/FixPrivateMethod.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using ADOFAI;
+using ADOFAI.LevelEditor.Controls;
 using JALib.Tools;
+using PropertyInfo = System.Reflection.PropertyInfo;
 
 namespace SmartEditor.FixLoad.CustomSaveState;
 
@@ -11,7 +13,14 @@ public static class FixPrivateMethod {
     public static MethodInfo CopyEventMethod = typeof(scnEditor).Method("CopyEvent");
     public static FieldInfo copiedHitsoundField = typeof(scnEditor).Field("copiedHitsound");
     public static FieldInfo copiedTrackColorField = typeof(scnEditor).Field("copiedHitsound");
-
+    public static MethodInfo updateSteamCallbacksMethod = typeof(scnEditor).Method("UpdateSteamCallbacks"); 
+    public static MethodInfo quitToMenuMethod = typeof(scnEditor).Method("QuitToMenu");
+    public static MethodInfo updateSelectedFloorMethod = typeof(scnEditor).Method("UpdateSelectedFloor");
+    public static MethodInfo ottoUpdateMethod = typeof(scnEditor).Method("OttoUpdate");
+    public static MethodInfo saveBackupMethod = typeof(scnEditor).Method("SaveBackup");
+    public static MethodInfo handleKeyboardActionsMethod = typeof(scnEditor).Method("HandleKeyboardActions");
+    public static MethodInfo handleMouseActionsMethod = typeof(scnEditor).Method("HandleMouseActions");
+    
     public static LevelEvent copiedHitsound => (LevelEvent) copiedHitsoundField.GetValue(scnEditor.instance);
     public static LevelEvent copiedTrackColor => (LevelEvent) copiedTrackColorField.GetValue(scnEditor.instance);
 
@@ -26,8 +35,29 @@ public static class FixPrivateMethod {
     public static void OffsetFloorIDsInEvents(int index, int offset) {
         OffsetFloorIDsInEventsMethod.Invoke(scnEditor.instance, [index, offset]);
     }
-
     public static LevelEvent CopyEvent(LevelEvent @event, int seqId) {
         return (LevelEvent) CopyEventMethod.Invoke(scnEditor.instance, [@event, seqId]);
+    }
+
+    public static void UpdateSteamCallbacks() {
+        updateSteamCallbacksMethod.Invoke(scnEditor.instance, null);
+    }
+    public static void QuitToMenu() {
+        quitToMenuMethod.Invoke(scnEditor.instance, null);
+    }
+    public static void UpdateSelectedFloor() {
+        updateSelectedFloorMethod.Invoke(scnEditor.instance, null);
+    }
+    public static void OttoUpdate() {
+        ottoUpdateMethod.Invoke(scnEditor.instance, null);
+    }
+    public static void SaveBackup() {
+        saveBackupMethod.Invoke(scnEditor.instance, null);
+    }
+    public static void HandleKeyboardActions() {
+        handleKeyboardActionsMethod.Invoke(scnEditor.instance, null);
+    }
+    public static void HandleMouseActions() {
+        handleMouseActionsMethod.Invoke(scnEditor.instance, null);
     }
 }
